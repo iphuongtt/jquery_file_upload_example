53a308699688119ee0223059b0fbc85afd0a9103
phuongtt <iphuongtt@gmail.com>
2014-08-02 10:25:07 +0700

    thay doi cau truc file

--- js/main.js
+++ js/main.js
@@ -73,3 +73,30 @@ $(function () {
     }
 
 });
+
+function include_template(tem_folder) {
+	var templates = ["template-download","template-upload"];
+	$.each(templates, function(index, value){
+		var template = $(value);
+		if (template.length === 0) {
+			var tmpl_dir = './template/'+ tem_folder;
+			var tmpl_url = tmpl_dir + '/' + value + '.tmpl';
+			var tmpl_string = '';
+			$.ajax({
+				url: tmpl_url,
+				method: 'GET',
+				async: false,
+				contentType: 'text',
+				success: function (data) {
+					tmpl_string = data;
+				}
+			});
+
+			$('head').append('<script id="' + 
+			value + '" type="text/x-tmpl">' + tmpl_string + '<\/script>');
+		}
+	});
+    
+}
+
+include_template('temp1');
